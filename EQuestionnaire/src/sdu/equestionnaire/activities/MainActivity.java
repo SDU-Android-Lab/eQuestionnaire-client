a2c533b3dd55e2981a97de24654b2dae9d7e9c1c
lihuiyu <huiyl91@163.com>
2012-07-23 03:26:23 +0800

    update

--- EQuestionnaire/src/sdu/equestionnaire/activities/MainActivity.java
+++ EQuestionnaire/src/sdu/equestionnaire/activities/MainActivity.java
@@ -18,14 +18,14 @@ public class MainActivity extends Activity {
 
 	private int mCenterX = 160;
 	private int mCenterY = 0;
-	private ViewGroup layoutA;
-	private ViewGroup layoutC;
-	private ViewGroup layoutB;
-	private ViewGroup layoutD;
+	private ViewGroup layoutFront;
+	private ViewGroup layoutBack;
+	private ViewGroup layoutRight;
+	private ViewGroup layoutLeft;
 
 	private SquareRotate leftAnimation;
 	private SquareRotate rightAnimation;
-	
+
 	private DisplayMetrics disManager;
 
 	/** Called when the activity is first created. */
@@ -38,10 +38,9 @@ public class MainActivity extends Activity {
 		final int screenWidth = disManager.widthPixels;
 		final int screenHeight = disManager.heightPixels;
 		mCenterX = screenWidth >> 1;
-		
-		
+
 		// 显示正面
-		layoutA = (ViewGroup) findViewById(R.id.layout_front);
+		layoutFront = (ViewGroup) findViewById(R.id.layout_front);
 		Button leftBtn = (Button) findViewById(R.id.front_leftBtn);
 		Button rightBtn = (Button) findViewById(R.id.front_rightBtn);
 		leftBtn.setEnabled(true);
@@ -49,20 +48,20 @@ public class MainActivity extends Activity {
 
 		leftBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallA_leftMoveHandle();
+				wallFront_leftMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 		rightBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallA_rightMoveHandle();
+				wallFront_rightMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 	}
 
 	// 左旋转
-	public void initFirst() {
+	private void initFirst() {
 		leftAnimation = new SquareRotate(0, -90, 0.0f, 0.0f, mCenterX, mCenterY);
 		rightAnimation = new SquareRotate(90, 0, 0.0f, 0.0f, mCenterX, mCenterY);
 		leftAnimation.setFillAfter(true);
@@ -72,7 +71,7 @@ public class MainActivity extends Activity {
 	}
 
 	// 右旋转
-	public void initSecond() {
+	private void initSecond() {
 		leftAnimation = new SquareRotate(-90, 0, 0.0f, 0.0f, mCenterX, mCenterY);
 		rightAnimation = new SquareRotate(0, 90, 0.0f, 0.0f, mCenterX, mCenterY);
 		leftAnimation.setFillAfter(true);
@@ -82,11 +81,10 @@ public class MainActivity extends Activity {
 	}
 
 	// B面转到A面所在位置
-	public void B2A(SquareRotate rightAnimation) {
+	private void right2front(SquareRotate rightAnimation) {
 		setContentView(R.layout.main_layout_right);
-		layoutB = (ViewGroup) findViewById(R.id.layout_right);
-		layoutB.startAnimation(rightAnimation);
-
+		layoutRight = (ViewGroup) findViewById(R.id.layout_right);
+		layoutRight.startAnimation(rightAnimation);
 		Button leftBtn = (Button) findViewById(R.id.right_leftBtn);
 		Button rightBtn = (Button) findViewById(R.id.right_rightBtn);
 		leftBtn.setEnabled(true);
@@ -94,235 +92,233 @@ public class MainActivity extends Activity {
 
 		leftBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallB_leftMoveHandle();
+				wallRight_leftMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 		rightBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallB_rightMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-	}
-
-	// D面转到A面所在位置
-	public void D2A(SquareRotate leftAnimation) {
-		setContentView(R.layout.main_layout_left);
-		layoutD = (ViewGroup) findViewById(R.id.layout_left);
-		layoutD.startAnimation(leftAnimation);
-
-		Button leftBtn = (Button) findViewById(R.id.left_leftBtn);
-		Button rightBtn = (Button) findViewById(R.id.left_rightBtn);
-		leftBtn.setEnabled(true);
-		rightBtn.setEnabled(true);
-
-		leftBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallD_leftMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-		rightBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallD_rightMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-	}
-
-	// A面转到D面所在位置
-	public void A2D(SquareRotate rightAnimation) {
-		setContentView(R.layout.main_layout_front);
-		layoutA = (ViewGroup) findViewById(R.id.layout_front);
-		layoutA.startAnimation(rightAnimation);
-
-		Button leftBtn = (Button) findViewById(R.id.front_leftBtn);
-		Button rightBtn = (Button) findViewById(R.id.front_rightBtn);
-
-		leftBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallA_leftMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-		rightBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallA_rightMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-	}
-
-	// C面转到D面所在位置
-	public void C2D(SquareRotate leftAnimation) {
-		setContentView(R.layout.main_layout_back);
-		layoutC = (ViewGroup) findViewById(R.id.layout_back);
-		layoutC.startAnimation(leftAnimation);
-
-		Button leftBtn = (Button) findViewById(R.id.back_leftBtn);
-		Button rightBtn = (Button) findViewById(R.id.back_rightBtn);
-
-		leftBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallC_leftMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-		rightBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallC_rightMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-	}
-
-	// C面转到B面所在位置
-	public void C2B(SquareRotate rightAnimation) {
-		setContentView(R.layout.main_layout_back);
-		layoutC = (ViewGroup) findViewById(R.id.layout_back);
-		layoutC.startAnimation(rightAnimation);
-
-		Button leftBtn = (Button) findViewById(R.id.back_leftBtn);
-		Button rightBtn = (Button) findViewById(R.id.back_rightBtn);
-
-		leftBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallC_leftMoveHandle();
-				v.setEnabled(false);
-			}
-		});
-		rightBtn.setOnClickListener(new Button.OnClickListener() {
-			public void onClick(View v) {
-				wallC_rightMoveHandle();
+				wallRight_rightMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 	}
 
 	// A面转到B面所在位置
-	public void A2B(SquareRotate leftAnimation) {
+	private void front2right(SquareRotate leftAnimation) {
 		setContentView(R.layout.main_layout_front);
-		layoutA = (ViewGroup) findViewById(R.id.layout_front);
-		layoutA.startAnimation(leftAnimation);
+		layoutFront = (ViewGroup) findViewById(R.id.layout_front);
+		layoutFront.startAnimation(leftAnimation);
 
 		Button leftBtn = (Button) findViewById(R.id.front_leftBtn);
 		Button rightBtn = (Button) findViewById(R.id.front_rightBtn);
 
 		leftBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallA_leftMoveHandle();
+				wallFront_leftMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 		rightBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallA_rightMoveHandle();
+				wallFront_rightMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+	}
+
+	// D面转到A面所在位置
+	private void left2front(SquareRotate leftAnimation) {
+		setContentView(R.layout.main_layout_left);
+		layoutLeft = (ViewGroup) findViewById(R.id.layout_left);
+		layoutLeft.startAnimation(leftAnimation);
+		Button leftBtn = (Button) findViewById(R.id.left_leftBtn);
+		Button rightBtn = (Button) findViewById(R.id.left_rightBtn);
+		leftBtn.setEnabled(true);
+		rightBtn.setEnabled(true);
+		leftBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallLeft_leftMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+		rightBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallLeft_rightMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+	}
+
+	// A面转到D面所在位置
+	private void front2left(SquareRotate rightAnimation) {
+		setContentView(R.layout.main_layout_front);
+		layoutFront = (ViewGroup) findViewById(R.id.layout_front);
+		layoutFront.startAnimation(rightAnimation);
+
+		Button leftBtn = (Button) findViewById(R.id.front_leftBtn);
+		Button rightBtn = (Button) findViewById(R.id.front_rightBtn);
+
+		leftBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallFront_leftMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+		rightBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallFront_rightMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+	}
+
+	// C面转到D面所在位置
+	private void back2left(SquareRotate leftAnimation) {
+		setContentView(R.layout.main_layout_back);
+		layoutBack = (ViewGroup) findViewById(R.id.layout_back);
+		layoutBack.startAnimation(leftAnimation);
+
+		Button leftBtn = (Button) findViewById(R.id.back_leftBtn);
+		Button rightBtn = (Button) findViewById(R.id.back_rightBtn);
+
+		leftBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallBack_leftMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+		rightBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallBack_rightMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 	}
 
 	// D面转到C面所在位置
-	public void D2C(SquareRotate rightAnimation) {
+	private void left2back(SquareRotate rightAnimation) {
 		setContentView(R.layout.main_layout_left);
-		layoutD = (ViewGroup) findViewById(R.id.layout_left);
-		layoutD.startAnimation(rightAnimation);
+		layoutLeft = (ViewGroup) findViewById(R.id.layout_left);
+		layoutLeft.startAnimation(rightAnimation);
 
 		Button leftBtn = (Button) findViewById(R.id.left_leftBtn);
 		Button rightBtn = (Button) findViewById(R.id.left_rightBtn);
 
 		leftBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallD_leftMoveHandle();
+				wallLeft_leftMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 		rightBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallD_rightMoveHandle();
+				wallLeft_rightMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+	}
+
+	// C面转到B面所在位置
+	private void back2right(SquareRotate rightAnimation) {
+		setContentView(R.layout.main_layout_back);
+		layoutBack = (ViewGroup) findViewById(R.id.layout_back);
+		layoutBack.startAnimation(rightAnimation);
+
+		Button leftBtn = (Button) findViewById(R.id.back_leftBtn);
+		Button rightBtn = (Button) findViewById(R.id.back_rightBtn);
+
+		leftBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallBack_leftMoveHandle();
+				v.setEnabled(false);
+			}
+		});
+		rightBtn.setOnClickListener(new Button.OnClickListener() {
+			public void onClick(View v) {
+				wallBack_rightMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 	}
 
 	// B面转到C面所在位置
-	public void B2C(SquareRotate leftAnimation) {
+	private void right2back(SquareRotate leftAnimation) {
 		setContentView(R.layout.main_layout_right);
-		layoutB = (ViewGroup) findViewById(R.id.layout_right);
-		layoutB.startAnimation(leftAnimation);
+		layoutRight = (ViewGroup) findViewById(R.id.layout_right);
+		layoutRight.startAnimation(leftAnimation);
 
 		Button leftBtn = (Button) findViewById(R.id.right_leftBtn);
 		Button rightBtn = (Button) findViewById(R.id.right_rightBtn);
 
 		leftBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallB_leftMoveHandle();
+				wallRight_leftMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 		rightBtn.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				wallB_rightMoveHandle();
+				wallRight_rightMoveHandle();
 				v.setEnabled(false);
 			}
 		});
 	}
 
 	// A位于正面时向左旋转 直到B位于正面
-	public void wallA_leftMoveHandle() {
+	private void wallFront_leftMoveHandle() {
 		initFirst();
-		layoutA.startAnimation(leftAnimation);
-		B2A(rightAnimation);
+		layoutFront.startAnimation(leftAnimation);
+		right2front(rightAnimation);
 	}
 
 	// A位于正面时向右旋转 直到D位于正面
-	public void wallA_rightMoveHandle() {
+	private void wallFront_rightMoveHandle() {
 		initSecond();
-		layoutA.startAnimation(rightAnimation);
-		D2A(leftAnimation);
+		layoutFront.startAnimation(rightAnimation);
+		left2front(leftAnimation);
 	}
 
 	// D位于正面时向左旋转 直到A位于正面
-	public void wallD_leftMoveHandle() {
+	private void wallLeft_leftMoveHandle() {
 		initFirst();
-		layoutD.startAnimation(leftAnimation);
-		A2D(rightAnimation);
+		layoutLeft.startAnimation(leftAnimation);
+		front2left(rightAnimation);
 	}
 
 	// D位于正面时向右旋转 直到C位于正面
-	public void wallD_rightMoveHandle() {
+	private void wallLeft_rightMoveHandle() {
 		initSecond();
-		layoutD.startAnimation(rightAnimation);
-		C2D(leftAnimation);
+		layoutLeft.startAnimation(rightAnimation);
+		back2left(leftAnimation);
 	}
 
 	// B位于正面时向左旋转 直到C位于正面
-	public void wallB_leftMoveHandle() {
+	private void wallRight_leftMoveHandle() {
 		initFirst();
-		layoutB.startAnimation(leftAnimation);
-		C2B(rightAnimation);
+		layoutRight.startAnimation(leftAnimation);
+		back2right(rightAnimation);
 	}
 
 	// B位于正面时向右旋转 直到A位于正面
-	public void wallB_rightMoveHandle() {
+	private void wallRight_rightMoveHandle() {
 		initSecond();
-		layoutB.startAnimation(rightAnimation);
-		A2B(leftAnimation);
+		layoutRight.startAnimation(rightAnimation);
+		front2right(leftAnimation);
 	}
 
 	// C位于正面时向左旋转 直到D位于正面
-	public void wallC_leftMoveHandle() {
+	private void wallBack_leftMoveHandle() {
 		initFirst();
-		layoutC.startAnimation(leftAnimation);
-		D2C(rightAnimation);
+		layoutBack.startAnimation(leftAnimation);
+		left2back(rightAnimation);
 	}
 
 	// C位于正面时向左旋转 直到B位于正面
-	public void wallC_rightMoveHandle() {
+	private void wallBack_rightMoveHandle() {
 		initSecond();
-		layoutC.startAnimation(rightAnimation);
-		B2C(leftAnimation);
+		layoutBack.startAnimation(rightAnimation);
+		right2back(leftAnimation);
 	}
 
 }
