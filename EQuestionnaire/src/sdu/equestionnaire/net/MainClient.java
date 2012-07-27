58c1b171373847d71548594e06ba8ac8994e88de
lihuiyu <huiyl91@163.com>
2012-07-27 14:24:49 +0800

    update

---
+++ EQuestionnaire/src/sdu/equestionnaire/net/MainClient.java
@@ -0,0 +1,56 @@
+package sdu.equestionnaire.net;
+
+import java.net.InetSocketAddress;
+
+import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.serialization.ObjectSerializationCodecFactory;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+public class MainClient {
+	public static ConnectFuture cf;
+	public static NioSocketConnector connector;
+
+	public void init() {
+		// Create TCP/IP connection
+
+		connector = new NioSocketConnector();
+
+		// ����������ݵĹ�����
+		DefaultIoFilterChainBuilder chain = connector.getFilterChain();
+
+		// �趨�����������һ��һ��(/r/n)�Ķ�ȡ���
+		chain.addLast("myChin", new ProtocolCodecFilter(
+				new ObjectSerializationCodecFactory()));
+
+		// ����������Ϣ��������һ��SamplMinaServerHander����
+		connector.setHandler(new SamplMinaClientHander());
+
+		// set connect timeout
+		connector.setConnectTimeout(30);
+
+		// ���ӵ���������
+		cf = connector.connect(new InetSocketAddress("localhost", 8899));
+
+		// Wait for the connection attempt to be finished.
+		cf.awaitUninterruptibly();
+
+	}
+
+	public void sendmes(Object obj) {
+
+		cf.getSession().write(obj);
+
+	}
+
+	public Object getMessage() {
+		return cf.getSession().read();
+	}
+
+	public void close() {
+		cf.getSession().getCloseFuture().awaitUninterruptibly();
+		connector.dispose();
+	}
+
+}
---
+++ EQuestionnaire/src/sdu/equestionnaire/net/SamplMinaClientHander.java
@@ -0,0 +1,45 @@
+package sdu.equestionnaire.net;
+
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+public class SamplMinaClientHander implements IoHandler {
+
+	public void exceptionCaught(IoSession arg0, Throwable arg1)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void messageReceived(IoSession arg0, Object arg1) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void messageSent(IoSession arg0, Object arg1) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sessionClosed(IoSession arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sessionCreated(IoSession arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sessionIdle(IoSession arg0, IdleStatus arg1) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sessionOpened(IoSession arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
