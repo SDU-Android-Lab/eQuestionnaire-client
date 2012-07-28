9f3107f232109f9f5806b6fe7b53ecb6600d078c
lihuiyu <huiyl91@163.com>
2012-07-28 15:24:29 +0800

    update

--- EQuestionnaire/src/sdu/equestionnaire/activities/RegisterActivity.java
+++ EQuestionnaire/src/sdu/equestionnaire/activities/RegisterActivity.java
@@ -3,8 +3,18 @@ package sdu.equestionnaire.activities;
 import sdu.equestionnaire.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
+	EditText register_nameE = (EditText) findViewById(R.id.account_name);
+	EditText register_phone_numberE = (EditText) findViewById(R.id.account_phoneNumber);
+	EditText register_provinceE = (EditText) findViewById(R.id.account_province);
+	EditText register_cityE = (EditText) findViewById(R.id.account_city);
+	EditText register_streetE = (EditText) findViewById(R.id.account_street);
+	EditText register_mailE = (EditText) findViewById(R.id.account_email);
+	Button register = (Button) findViewById(R.id.account_btn_apply);
+	Button cancel = (Button) findViewById(R.id.account_btn_cancel);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
